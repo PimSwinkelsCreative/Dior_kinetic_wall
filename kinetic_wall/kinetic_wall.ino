fac4d61e6e8675b7d54a85a0c31b59f77d042701
Pim Swinkels <contact@pimsiwnkels.com>
2023-03-27 16:53:25 +0200

    first animation installed, simple back and forth with an offset

--- kinetic_wall/kinetic_wall.ino
+++ kinetic_wall/kinetic_wall.ino
@@ -24,25 +24,58 @@ void setup() {
 
 
   // perform a homing procedure on startup:
-  #ifndef SKIP_HOMING
+#ifndef SKIP_HOMING
   for (int i = 0;i < nMotors;i++) {
-    startMotorHoming(i,true);
+    startMotorHoming(i, true);
   }
-  #endif
+#endif
 }
 
 void loop() {
   unsigned long startTime = micros();
-  if (millis() - lastPositionUpdate > 2000) {
-    lastPositionUpdate = millis();
-    for (int i = 0; i < nMotors; i++) {
-      moveMotorToPosition(i, float(random(-100, 100)) / 100.0, 1,5);
-    }
-  }
+  // if (millis() - lastPositionUpdate > 2000) {
+  //   lastPositionUpdate = millis();
+  //   for (int i = 0; i < nMotors; i++) {
+  //     moveMotorToPosition(i, float(random(-100, 100)) / 100.0, 1, 5);
+  //   }
+  // }
 
   // // update the steppermotors, update as often as possible!
   // updateMotors();
 
-  unsigned long loopTime = micros()-startTime;
+  if (millis() - lastPositionUpdate > 10) {
+    playAnimation(0);
+  }
+
+  unsigned long loopTime = micros() - startTime;
   // Serial.println("looptime: "+String(loopTime)+"us");
+}
+
+void playAnimation(uint8_t currentAnimation) {
+
+  switch (currentAnimation) {
+  case 0: {
+    int animationDuration = 8000;  //animation duration in milliseconds
+    float motorOffsets[nMotors];
+    for (int i = 0;i < nMotors;i++) {
+      motorOffsets[i] = float(i) / float(nMotors);
+    }
+    float animationProgress = float(millis() % animationDuration) / float(animationDuration);
+    for (int i = 0;i < nMotors;i++) {
+      float pos = animationProgress+motorOffsets[i];
+      if(pos>1) pos-=1;
+      pos = pos*2;
+      if(pos>1) pos = 2-pos;
+      moveMotorToPosition(i, pos, 10, 10);
+    }
+    break;
+  } 
+  case 1:
+  case 2:
+  case 3:
+  default:
+    Serial.println("ERROR: animation out of range!");
+    break;
+
+  }
 }
